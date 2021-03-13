50f182997d8288428a4ee612147c74dfc1789c47
chairylin <chairylin@mail.ru>
2021-03-13 16:51:57 +0300

    create lang file

---
+++ lang/ru/install/index.php
@@ -0,0 +1,4 @@
+<?
+$MESS ['CHAIRYLIN_NOTIFICATIONS_INSTALL_NAME'] = "Уведомления";
+$MESS ['CHAIRYLIN_NOTIFICATIONS_INSTALL_DESCRIPTION'] = "Синхронизация с внешним сервисом. Предоставление уведомлений пользователям";
+?>
